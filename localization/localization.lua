928c721ed813f25432bfcfbe9dc31102075e5787
Brett <Brett@DESKTOP-GNB03I7>
2016-12-29 21:51:39 +1100

    First commit

---
+++ localization/localization.lua
@@ -0,0 +1,28 @@
+--[[
+	Localization.lua
+		Translations for MountMiniMe
+
+	English: Default language
+--]]
+
+local L = LibStub('AceLocale-3.0'):NewLocale('MountMiniMe-Config', 'enUS', true)
+
+L.Set = 'Set'
+L.Save = 'Save'
+L.Copy = 'Copy'
+L.Delete = 'Delete'
+L.EnterName = 'Enter Name'
+L.Profiles = 'Profiles'
+L.ProfilesPanelDesc = 'Allows you to manage saved MountMiniMe layouts'
+L.ShowMinimapButton = 'Show Minimap Button'
+
+--general settings panel
+L.General = 'General'
+
+--profile settings panel
+L.CreateProfile = 'Create Profile...'
+L.ResetProfile = 'Reset Profile...'
+L.CopyProfile = 'Copy Profile...'
+L.ConfirmResetProfile = 'Are you sure you want to reset your profile?'
+L.ConfirmCopyProfile = 'Copy contents from %s into the current profile?'
+L.ConfirmDeleteProfile = 'Delete profile %s?'
---
+++ panels/general.lua
@@ -0,0 +1,25 @@
+--[[
+
+	general.lua
+
+		A gui for general addon configuration settings
+
+--]]
+
+local AddonName, Addon = ...
+local ParentAddonName, ParentAddon = GetAddOnDependencies(AddonName)
+local ParentAddon = _G[ParentAddonName]
+local L = LibStub('AceLocale-3.0'):GetLocale(ParentAddonName .. '-Config')
+
+local GeneralPanel = Addon.AddonOptions:NewPanel(L.General)
+do
+  --[[ General Settings ]]--
+
+  local showMinimapButtonToggle = GeneralPanel:Add('CheckButton', {
+    name = L.ShowMinimapButton,
+    get = function() return ParentAddon:ShowingMinimap() end,
+    set = function(_, enable) ParentAddon:SetShowMinimap(enable) end
+  })
+  showMinimapButtonToggle:SetPoint('TOPLEFT', 0, -2)
+
+end
